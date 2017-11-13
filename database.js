ea55f5a997ee4d858141b11537f380cd14dc831e
Majid Daghagheleh <majid.dagha@gmail.com>
2017-11-13 19:57:47 +1300

    1. npm installed dotenv module 2. created .env file 3. added .env file to .gitignore 4.used process.env variables in database.js file

--- database.js
+++ database.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
 var knex = require('knex')({
   client: 'mssql',
   connection: {
-    host: '127.0.0.1',
-    user: 'SA',
-    password: 'Majid@2013',
-    database: 'Todoapp'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
   }
 });
 
-knex.schema.createTable('Todos', function(table) {
-  table.string('taskname').primary();
-  table.string('taskdescription');
-  table.boolean('taskdone').defaultTo(false);
+knex.schema.createTable(process.env.TABLE_NAME, function(table) {
+  table.string(process.env.TABLE_TITLE_COLUMN).primary();
+  table.string(process.env.TABLE_DESCRIPTION_COLUMN);
+  table.date(process.env.TABLE_DUE_DATE_COLUMN);
+  table.boolean(process.env.TABLE_DONE_COLUMN).defaultTo(false);
 }).then();
